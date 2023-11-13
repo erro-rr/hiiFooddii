05a24d237e7b7da9d621084d3d4f141f10bf1dd5
Devendra Singh <devendrasahabsingh4522@gmail.com>
2023-11-13 09:34:44 +0530

    proxy issue ig solved

--- src/Contants.js
+++ src/Contants.js
@@ -2,7 +2,7 @@ export const IMG_CDN_URL="https://res.cloudinary.com/swiggy/image/upload/fl_loss
 
 // Swiggy API to get Restaurant data with corsproxy
 export const swiggy_api_URL =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.689214&lng=77.2239895&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.689214&lng=77.2239895&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const ITEM_IMG_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
